7adefadfda9d1cfe5f5a78cb551f20e792a10945
rezig <gelel.rezig@etud.univ-jfc.fr>
2018-12-10 17:44:12 +0100

    classpolice

---
+++ src/main/java/bandeau/Police.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bandeau;
+import java.awt.Font;
+/**
+ *
+ * @author greziz
+ */
+public class Police extends Animations{
+    private String poli;
+    private int tail;
+    public Police(String police, int taille){
+        poli=police;
+        tail=taille;   
+    }
+        @Override
+    public void effectuer() {
+        myBandeau.setFont(new Font(poli, Font.PLAIN, tail));
+}
+}
